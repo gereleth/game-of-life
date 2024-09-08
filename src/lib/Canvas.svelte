8bc8a5d34769f48d367afd5e514a13ec5bfc5d66
Daria Vasyukova <daria.voznyak@gmail.com>
2024-09-08 10:05:02 +0300

    Make touch event handlers non-passive prevents whole-page pinch-zoom on mobile

--- src/lib/Canvas.svelte
+++ src/lib/Canvas.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { onMount, setContext, getContext } from 'svelte';
 
-    let {children, ...eventHandlers} = $props()
+    let {children, ontouchstart, ontouchmove, ontouchend, ...eventHandlers} = $props()
 
 	/** @type {CanvasRenderingContext2D|null}*/
 	let ctx=$state(null);
@@ -14,6 +14,19 @@
 
 	onMount(() => {
 		ctx = canvas.getContext('2d');
+		// Manually adding listeners here to prevent whole-page pinch-zoom gestures
+		// I want only the canvas part to zoom, not the controls and buttons
+		canvas.addEventListener('touchstart', ontouchstart, {passive:false})
+		canvas.addEventListener('touchmove', ontouchmove, {passive:false})
+		canvas.addEventListener('touchend', ontouchend, {passive:false})
+		canvas.addEventListener('touchcancel', ontouchend, {passive:false})
+
+		return () => {
+			canvas.removeEventListener('touchstart', touchstart)
+			canvas.removeEventListener('touchmove', touchmove)
+			canvas.removeEventListener('touchend', touchend)
+			canvas.removeEventListener('touchcancel', touchcancel)
+		}
 	});
 </script>
 
